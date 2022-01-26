05a5be6a6450d6dd9055bfdc9d719ba59f6e3e3b
eka-wq <51916016+eka-wq@users.noreply.github.com>
2022-01-26 20:29:40 +0700

    Update values.js

--- src/config/values.js
+++ src/config/values.js
@@ -14,6 +14,6 @@ exports.subject_email = "Pemalas APi | Verification";
  * Configurasi database
  */
 exports.mongoUrl =
-  "mongodb+srv://pemalasapi:<password>@cluster0.krrrk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://pemalasapi:ekaaditya17@cluster0.krrrk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 exports.dbGet = "pemalasapi";
 exports.msgSuccesConnect = "Connected correctly to server";
